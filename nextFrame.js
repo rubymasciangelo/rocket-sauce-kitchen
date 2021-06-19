b69d7411c2057032480cf6894d6d4640abadd2fd
rubymasciangelo <rubymasciangelo@gmail.com>
2021-06-19 00:56:59 -0700

    demo1: sizing fixed, layering images fixed

--- nextFrame.js
+++ nextFrame.js
@@ -1,33 +1,80 @@
 const html = document.documentElement;
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
+
 const frameCount = 192;
 
-//get the scroll position (frameIndex) and attribute it to an index position from the animation frame sequence
-window.addEventListener('scroll', () => {  
-    const scrollTop = html.scrollTop;
-    const maxScrollTop = html.scrollHeight - window.innerHeight;
-    const scrollFraction = scrollTop / maxScrollTop;
-    const frameIndex = Math.min(
-        frameCount - 1,
-        Math.floor(scrollFraction * frameCount)
-        );
-    });
 
+//gets the image from the directory, padstart handles the 000 naming aspect
+const currentFrame = index => (
+    `./Exhaust-Sequence/RocketExhaust${index.toString().padStart(3, '0')}.png`
+)
 
-    var eFrames = document.querySelectorAll("img.frameimg");
-
-
-function nextFrame() {
-    var i
-    for (i = 0; i < eFrames.length; ++i) {
-        e = eFrames[i];
-        if (e.index === frameIndex) {
-        e.style.opacity = "1";
-        }
-        else {
-            e.style.opacity = "0";
-        }
+const preloadImages = () => {
+    for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
     }
+};    
+
+const img = new Image()
+    img.src = currentFrame(1);
+    canvas.width=1158;
+    canvas.height=770;
+    img.onload=function(){
+    context.drawImage(img, 0, 0, img.width, img.height, //source rectangle
+                            0, 0, canvas.width, canvas.height); //destination rectangle
 }
 
-    //for each item in the image list if the image index matches
-    //the framecount then append the child or style opacity to 1
+const updateImage = index => {
+    img.src = currentFrame(index);
+    context.drawImage(img, 0, 0, img.width, img.height, //source rectangle
+        0, 0, canvas.width, canvas.height); //destination rectangle
+}
+
+//get the scroll position and attribute it to an index position from the animation frame sequence
+window.addEventListener('scroll', () => {  
+const scrollTop = html.scrollTop;
+const maxScrollTop = html.scrollHeight - window.innerHeight;
+const scrollFraction = scrollTop / maxScrollTop;
+const frameIndex = Math.min(
+    frameCount - 1,
+    Math.floor(scrollFraction * frameCount)
+    );
+
+//update image source and draw new image on canvas
+requestAnimationFrame(() => updateImage(frameIndex + 1))
+context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+});
+
+
+preloadImages();
+
+//this works but the images paint over and
+//over on themselves and since the background
+//is transparent that is not the vibe. ALSO
+//something bad is going on with the sizing?
+
+
+//this is the only thing that needs to be in body
+//<canvas id="hero-lightpass" />
+
+
+//html {
+ //   height: 100vh;
+  //}
+  
+  /* taller body to give us enough scroll room */
+ // body {
+   // height: 500vh;
+ // }
+  
+  /* does not exceed dimension of viewport */
+ // canvas {
+   // position: fixed;
+  //  left: 50%;
+    //top: 50%;
+   // max-height: 100vh;
+   // max-width: 50vw;
+   // transform: translate(-50%, -50%);
+ // }
