0201ab1254e27060ed1feb70b9ad030361d4d9dd
rubymasciangelo <rubymasciangelo@gmail.com>
2021-07-10 21:16:56 -0700

    ?

--- ldScript.js
+++ ldScript.js
@@ -1,6 +1,5 @@
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('anim'),
-    renderer: 'svg',
     path: 'Rocket-Exhaust.json',
     mode: 'scroll',
     actions: [
