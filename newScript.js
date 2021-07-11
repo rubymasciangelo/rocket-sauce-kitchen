0054dd525f4c6c264a38b5dafefcf75a00017746
rubymasciangelo <rubymasciangelo@gmail.com>
2021-07-10 22:22:25 -0700

    newdemo

---
+++ newScript.js
@@ -0,0 +1,41 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var elem = document.getElementById('blastoff');
+
+
+LottieScrollTrigger({
+  target: "#blastoff",
+  path: "Rocket-Exhaust.json",
+  speed: "medium",
+  pin: ".main",
+  start: "center center",
+  end: () => `+=${elem.offsetHeight}`,
+  scrub: 1,
+  markers: true
+});
+
+
+function LottieScrollTrigger(vars) {
+	let playhead = {frame: 0},
+		target = gsap.utils.toArray(vars.target)[0],
+		speeds = {slow: "+=2000", medium: "+=1000", fast: "+=500"},
+		st = {trigger: target, pin: true, start: "top top", end: speeds[vars.speed] || "+=1000", scrub: 1},
+		animation = lottie.loadAnimation({
+			container: target,
+			renderer: vars.renderer || "svg",
+			loop: false,
+			autoplay: false,
+			path: vars.path
+		});
+	for (let p in vars) { // let users override the ScrollTrigger defaults
+		st[p] = vars[p];
+	}
+	animation.addEventListener("DOMLoaded", function() {
+		gsap.to(playhead, {
+			frame: animation.totalFrames - 1,
+			ease: "none",
+			onUpdate: () => animation.goToAndStop(playhead.frame, true),
+			scrollTrigger: st
+		});
+	});
+}
