8ab2f781a18335b9cc1255b8440a287f7c9db866
rubymasciangelo <rubymasciangelo@gmail.com>
2021-08-08 16:58:50 -0700

    speed options

---
+++ fastestScript.js
@@ -0,0 +1,47 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var elem = document.getElementById('blastoff');
+var hit = document.getElementsByClassName('main');
+
+
+LottieScrollTrigger({
+  target: "#blastoff",
+  path: "RocketSauce_Full V2.json",
+  speed: "slow",
+  pin: ".main",
+  anticipatePin: true,
+  start: "top",
+  end: () => `+=${elem.offsetHeight * 30}`,
+  scrub: 1,
+  rotation: 0.1,
+  force3D: true,
+  markers: false
+});
+
+
+function LottieScrollTrigger(vars) {
+	let playhead = {frame: 0},
+		target = gsap.utils.toArray(vars.target)[0],
+		speeds = {slow: "+=8000", medium: "+=1000", fast: "+=500"},
+		st = {trigger: target, pin: true, start: "top top", end: speeds[vars.speed] || "+=8000", scrub: 1},
+		animation = lottie.loadAnimation({
+			container: target,
+			renderer: vars.renderer || "svg",
+			loop: false,
+			autoplay: false,
+			path: vars.path,
+			rotation: 0.1,
+			force3D: true
+		});
+	for (let p in vars) { // let users override the ScrollTrigger defaults
+		st[p] = vars[p];
+	}
+	animation.addEventListener("DOMLoaded", function() {
+		gsap.to(playhead, {
+			frame: animation.totalFrames - 1,
+			ease: "none",
+			onUpdate: () => animation.goToAndStop(playhead.frame, true),
+			scrollTrigger: st
+		});
+	});
+}
