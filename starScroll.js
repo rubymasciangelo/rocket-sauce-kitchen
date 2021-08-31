5fc1d7410a6dbc4cc81efffd1c96e15abdeadfb5
rubymasciangelo <rubymasciangelo@gmail.com>
2021-08-30 18:35:45 -0700

    star parallax first attempt simple parallax

---
+++ starScroll.js
@@ -0,0 +1,53 @@
+gsap.to(".star-6", {
+    yPercent: -300,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".landing-entry",
+        scrub: true
+    },
+});
+
+gsap.to(".star-5", {
+    yPercent: -80,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".landing-entry",
+        scrub: true
+    },
+});
+
+gsap.to(".star-4", {
+    yPercent: -70,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".landing-entry",
+        scrub: true
+    },
+});
+
+gsap.to(".star-3", {
+    yPercent: -30,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".landing-entry",
+        scrub: true
+    },
+});
+
+gsap.to(".star-2", {
+    yPercent: -50,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".landing-entry",
+        scrub: true
+    },
+});
+
+gsap.to(".star-1", {
+    yPercent: -50,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".landing-entry",
+        scrub: true
+    },
+});
