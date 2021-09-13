d7fe49ff885fc2fa29fd89dbeda328a4bbccc760
rubymasciangelo <rubymasciangelo@gmail.com>
2021-09-13 15:38:10 -0700

    9:16 and 9:21 set up attempt

---
+++ lottie21.js
@@ -0,0 +1,47 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var elem = document.getElementById('blastoff-21');
+var hit = document.getElementsByClassName('main-21');
+
+
+LottieScrollTrigger({
+  target: "#blastoff-21",
+  path: "RocketSauce_9x21 V1.json",
+  speed: "fast",
+  pin: ".main-21",
+  anticipatePin: true,
+  start: "top",
+  end: () => `+=${elem.offsetHeight * 5}`,
+  scrub: 1,
+  rotation: 0.1,
+  force3D: true,
+  markers: false
+});
+
+
+function LottieScrollTrigger(vars) {
+	let playhead = {frame: 0},
+		target = gsap.utils.toArray(vars.target)[0],
+		speeds = {slow: "+=2500", medium: "+=1000", fast: "+=50"},
+		st = {trigger: target, pin: true, start: "top top", end: speeds[vars.speed] || "+=50", scrub: 1},
+		animation = lottie.loadAnimation({
+			container: target,
+			renderer: vars.renderer || "svg",
+			loop: false,
+			autoplay: false,
+			path: vars.path,
+			rotation: 0.1,
+			force3D: true
+		});
+	for (let p in vars) { // let users override the ScrollTrigger defaults
+		st[p] = vars[p];
+	}
+	animation.addEventListener("DOMLoaded", function() {
+		gsap.to(playhead, {
+			frame: animation.totalFrames - 1,
+			ease: "none",
+			onUpdate: () => animation.goToAndStop(playhead.frame, true),
+			scrollTrigger: st
+		});
+	});
+}
