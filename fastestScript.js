a16d3b084a29eecc058090346bd86354ca2e8215
rubymasciangelo <rubymasciangelo@gmail.com>
2021-10-04 18:14:09 -0700

    Revert "undo starscroll changes"
    This reverts commit 7a30922a0c61cbf59db3463dbbf9822c3ccab9c1.

--- fastestScript.js
+++ fastestScript.js
@@ -45,3 +45,79 @@ function LottieScrollTrigger(vars) {
 		});
 	});
 }
+
+
+var elemStar = document.getElementById('stars');
+
+
+gsap.from(".star-6", {
+    y: "-100px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1.5}`,
+        end: () => `+=${elemStar.offsetHeight * 2.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-5", {
+    y: "-50px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1.5}`,
+        end: () => `+=${elemStar.offsetHeight * 2.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-4", {
+    y: "-50px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1.5}`,
+        end: () => `+=${elemStar.offsetHeight * 2.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-3", {
+    y: "-15px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1.5}`,
+        end: () => `+=${elemStar.offsetHeight * 2.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-2", {
+    y: "-30px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1.5}`,
+        end: () => `+=${elemStar.offsetHeight * 2.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-1", {
+    y: "50px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1.5}`,
+        end: () => `+=${elemStar.offsetHeight * 2.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
--- lottie21.js
+++ lottie21.js
@@ -11,7 +11,7 @@ LottieScrollTrigger({
   pin: ".main-21",
   anticipatePin: true,
   start: "top",
-  end: () => `+=${elem.offsetHeight * 2}`,
+  end: () => `+=${elem.offsetHeight * 4}`,
   scrub: 1,
   rotation: 0.1,
   force3D: true,
@@ -45,3 +45,77 @@ function LottieScrollTrigger(vars) {
 		});
 	});
 }
+
+var elemStar = document.getElementById('stars');
+
+gsap.from(".star-6", {
+    y: "-100px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1}`,
+        end: () => `+=${elemStar.offsetHeight * 1.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-5", {
+    y: "-50px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1}`,
+        end: () => `+=${elemStar.offsetHeight * 1.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-4", {
+    y: "-50px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1}`,
+        end: () => `+=${elemStar.offsetHeight * 1.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-3", {
+    y: "-15px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1}`,
+        end: () => `+=${elemStar.offsetHeight * 1.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-2", {
+    y: "-30px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1}`,
+        end: () => `+=${elemStar.offsetHeight * 1.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
+
+gsap.from(".star-1", {
+    y: "50px",
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#stars",
+        start: () => `+=${elemStar.offsetHeight * 1}`,
+        end: () => `+=${elemStar.offsetHeight * 1.5}`,
+        scrub: 2,
+        // markers: true
+    },
+});
