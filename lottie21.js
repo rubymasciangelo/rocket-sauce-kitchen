fd5247f69696698cd5f8bdf9fc62bcea0a3eb58b
rubymasciangelo <rubymasciangelo@gmail.com>
2021-10-04 18:15:05 -0700

    Revert "JSON replaced with lower size"
    This reverts commit 057b8c00d48e45970f23c57ca48ad2d542c653c3.

--- lottie21.js
+++ lottie21.js
@@ -6,7 +6,7 @@ var hit = document.getElementsByClassName('main-21');
 
 LottieScrollTrigger({
   target: "#blastoff-21",
-  path: "LRS21.json",
+  path: "RocketSauce_9x21 V1.json",
   speed: "fast",
   pin: ".main-21",
   anticipatePin: true,
