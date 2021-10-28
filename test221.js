2eed0c9c14db6db2d48917b3ba9107e38dbf0ddd
rubymasciangelo <rubymasciangelo@gmail.com>
2021-10-27 18:57:31 -0700

    png folders renamed, 16 and 21 js created, linked

---
+++ test221.js
@@ -0,0 +1,44 @@
+console.clear();
+
+const canvas = document.getElementById("hero-lightpass");
+const context = canvas.getContext("2d");
+
+// canvas.width = document.body.clientWidth;
+// canvas.height = document.body.clientHeight;
+
+canvas.width = 1920;
+canvas.height = 1080;
+
+const frameCount = 263;
+const currentFrame = index => (
+    `./pngs/21A/RocketSauce_9x21${index.toString().padStart(3, '0')}.png`
+);
+
+const images = []
+const rocket = {
+  frame: 0
+};
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  images.push(img);
+}
+
+gsap.to(rocket, {
+  frame: frameCount - 1,
+  snap: "frame",
+  scrollTrigger: {
+    end: () => `+=${canvas.offsetHeight * 5}`,
+    scrub: 0.5
+  },
+  onUpdate: render // use animation onUpdate instead of scrollTrigger's onUpdate
+});
+
+images[0].onload = render;
+
+function render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(images[rocket.frame], 0, 0, images[rocket.frame].width, images[rocket.frame].height, //source rectangle
+    0, 0, canvas.width, canvas.height); //destination rectangle
+}
